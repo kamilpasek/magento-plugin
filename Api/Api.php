4bdf1e3403d4dd11c93f88cd4758ce8d10dea303
kamilpasek <kamil.pasek@ipresso.com>
2023-10-11 13:15:58 +0200

    init

---
+++ Api/Api.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ipresso\Magento\Api;
+
+class Api
+{
+
+}
---
+++ registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Ipresso_MagentoIntegration', __DIR__);
