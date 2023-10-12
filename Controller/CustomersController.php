fd948a1ddabaf24f66edac74e2ad06eed4408807
kamilpasek <kamil.pasek@ipresso.com>
2023-10-12 07:26:45 +0200

    add response to controller

--- Controller/CustomersController.php
+++ Controller/CustomersController.php
@@ -24,7 +24,7 @@ class CustomersController implements ActionInterface,
             throw new AuthorizationException(__('Please log in'));
         }
         // code
-
+        return json_encode(['success' => true]);
     }
 
 }
